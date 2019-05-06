fa769d5db9a05dac94a13eac7d303f85083af8e5
Sean Chen <seanchen513@gmail.com>
2019-05-05 21:29:35 -0700

    Update comments

--- dcp119 - given set of closed intervals, find smallest set of numbers that covers all intervals.py
+++ dcp119 - given set of closed intervals, find smallest set of numbers that covers all intervals.py
@@ -1,12 +1,20 @@
 """
 dcp#119
-dcp#200
 
 This problem was asked by Google.
 
 Given a set of closed intervals, find the smallest set of numbers that covers all the intervals. If there are multiple smallest sets, return any of them.
 
 For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], one set of numbers that covers all these intervals is {3, 6}.
+
+//
+dcp#200
+
+This problem was asked by Microsoft.
+
+Let X be a set of n intervals on the real line. We say that a set of points P "stabs" X if every interval in X contains at least one point in P. Compute the smallest set of points that stabs X.
+
+For example, given the intervals [(1, 4), (4, 5), (7, 9), (9, 12)], you should return [4, 9].
 """
 
 # Assume intervals are closed.
@@ -36,8 +44,8 @@ intervals = [ [0, 1] ] # trivial example
 intervals = [ [0,4], [0,3], [0,2], [0,1] ] # possible covers [0], ..., [1]
 intervals = [ [0,7], [1,6], [2,5], [3,4] ] # possible covers = [3], ..., [4]
 
-intervals = [ [0, 3], [2, 6], [3, 4], [6, 9] ] # possible covers = [3, 6], ..., [3, 9]
-intervals = [ [1, 4], [4, 5], [7, 9], [9, 12] ] # [4, 9]
+intervals = [ [0, 3], [2, 6], [3, 4], [6, 9] ] # dcp119; possible covers = [3, 6], ..., [3, 9]
+intervals = [ [1, 4], [4, 5], [7, 9], [9, 12] ] # dcp200; [4, 9]
 
 print("\nintervals = {}".format(intervals))
 
