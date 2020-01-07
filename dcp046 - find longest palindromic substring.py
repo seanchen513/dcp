1f96dedd1eb50a8c8ca3a0e66ac501f642d0fdff
Sean Chen <seanchen513@gmail.com>
2020-01-06 16:17:24 -0800

    dcp46 - add variation that passes start and end indices rather than substrings

--- dcp046 - find longest palindromic substring.py
+++ dcp046 - find longest palindromic substring.py
@@ -56,8 +56,6 @@ def longest_palindrome(s):
 # Solution #2:
 # Essentially the same solution, but combine the methods used
 # in the first solution.
-# Could modify this to return the starting index of the palindromic
-# substring...
 def longest_palindrome2(s):
     start = 0
     end = len(s) - 1
@@ -80,10 +78,36 @@ def longest_palindrome2(s):
         return l2, s2
 
 
+# Solution #3:
+# Same as #2 but pass start and end indices for substrings to check
+# rather than passing substrings themselves.
+def longest_palindrome3(s, start, end):
+    start2 = start
+    end2 = end
+
+    # check if "s" is a palindrome
+    while (start2 <= end2)  and (s[start2] == s[end2]):
+        start2 += 1
+        end2 -= 1
+
+    if start2 > end2: # "s" is a palindrome
+        return start, end
+
+    # "s" is not a palindrome, so...
+    pal_start1, pal_end1 = longest_palindrome3(s, start + 1, end)
+    pal_start2, pal_end2 = longest_palindrome3(s, start, end - 1)
+
+    if pal_end1 - pal_start1 > pal_end2 - pal_start2:
+        return pal_start1, pal_end1
+    else:
+        return pal_start2, pal_end2
+
+
 ###############################################################################
 
 
 strings = ["", "a", "ab", "abc",
+    "abb", "baa",
     "elgoog", "google", "apple", "facebook",
     "coffee", "nancy", "pizza", "apple",
     "bosonic", "photon",
@@ -93,9 +117,16 @@ strings = ["", "a", "ab", "abc",
 for s in strings:
     #p = longest_palindrome_length(s)
     
+    ###
     #p = longest_palindrome(s)
     #print("{}, {}".format(s, p))
     
-    length, pal = longest_palindrome2(s)
-    print("{}, {}, {}".format(s, length, pal))
+    ###
+    # length, pal = longest_palindrome2(s)
+    # print("{}, {}, {}".format(s, length, pal))
     
+    ###
+    start, end = longest_palindrome3(s, 0, len(s) - 1)
+    length = end - start + 1
+    print("{}, {}, {}, {}".format(s, length, s[start:end+1], start))
+
