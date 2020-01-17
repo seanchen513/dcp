9e72f9f85ab832078b2c165c7f35101d0c2798c3
Sean Chen <seanchen513@gmail.com>
2020-01-16 21:16:21 -0800

    update dcp026

--- linked_list/dcp026_remove_kth_last_node_from_LL.py
+++ linked_list/dcp026_remove_kth_last_node_from_LL.py
@@ -1,5 +1,17 @@
 """
-dcp#398
+dcp26
+
+This problem was asked by Google.
+
+Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length of the list.
+
+The list is very long, so making more than one pass is prohibitively expensive.
+
+Do this in constant space and in one pass.
+
+###############################################################################
+
+dcp398
 
 This problem was asked by Amazon.
 
@@ -8,22 +20,68 @@ Given a linked list and an integer k, remove the k-th node from the end of the l
 k is guaranteed to be smaller than the length of the list.
 
 Do this in one pass.
-
 """
 
+"""
+LC19. Remove Nth Node From End of List
+Medium
+
+Given a linked list, remove the n-th node from the end of list and return its head.
+
+Example:
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+
+Note:
+Given n will always be valid.
+
+Follow up:
+Could you do this in one pass?
+"""
 
 class Node():
-    def __init__(self, val, next=None):
+    def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
     def __repr__(self):
         return f"{self.val}, {self.next.__repr__()}"
 
+"""
+Solution:
+Assume k is integer >= 1, and <= length of linked list.
 
-# Assume k is integer >= 1, and <= length of linked list.
-# O(n) time, one pass
+O(n) time, one pass
+"""
 def remove_kth_from_end(head, k):
+    # Advance scout by k first.
+    scout = head
+
+    for _ in range(k):
+        scout = scout.next
+    
+    if scout is None: # k == length of list, so remove head node
+        return head.next
+
+    # scout is now ahead of head (lag) by k.
+    lag = head
+
+    while scout.next:
+        scout = scout.next
+        lag = lag.next
+
+    # scout is at end of list, and lag is k behind,
+    # so lag is behind the node we want to remove.
+    lag.next = lag.next.next
+
+    return head
+
+"""
+Same solution without assumptions about k.
+"""
+def remove_kth_from_end2(head, k):
     if k < 1:
         print("Given k must be an integer >= 1.")
         return head # don't modify linked list
@@ -56,33 +114,37 @@ def remove_kth_from_end(head, k):
 
     return head
 
-
 ###############################################################################
 
-head = Node(1, # 9th from end; k = 1 to remove it
-    Node(2,
-    Node(3,
-    Node(4,
-    Node(5,
-    Node(6,
-    Node(7,
-    Node(8, # 2nd from end; k = 2 to remove it
-    Node(9, # 1st from end
-    )))))))))
+if __name__ == "__main__":
+    import copy
+    def test(head, k):    
+        print("#"*80)
+        print(f"\n{head}")
 
-k = 0 # error: k must be >= 1
-k = 1 # should remove end of linked list
-k = 9 # should remove current head node
-k = 10 # error: k must be <= length of linked list
+        print(f"\nk = {k}, removing kth node from end:")
+        
+        head = remove_kth_from_end(head, k)
+        #print("\nAfter removing kth from end:")
+        print(head)
+        print()
 
-k = 3
 
-print("\nLinked list:")
-print(head)
+    head = Node(1, # 9th from end; k = 1 to remove it
+        Node(2,
+        Node(3,
+        Node(4,
+        Node(5,
+        Node(6,
+        Node(7,
+        Node(8, # 2nd from end; k = 2 to remove it
+        Node(9, # 1st from end
+        )))))))))
 
-print(f"\nk = {k}")
+    #test(copy.deepcopy(head), 0) # error: k must be >= 1
+    #test(copy.deepcopy(head), 10) # error: k must be <= length of linked list
 
-head = remove_kth_from_end(head, k)
-print("\nAfter removing kth from end of linked list:")
-print(head)
-
+    test(copy.deepcopy(head), 2)
+    test(copy.deepcopy(head), 1) # should remove end of linked list
+    test(copy.deepcopy(head), 9) # should remove current head node
+    
