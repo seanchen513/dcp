dc4790e04a6b32ea87087ab718a4d622be21ccf3
Sean Chen <seanchen513@gmail.com>
2020-01-28 20:35:15 -0800

    update dcp394

--- tree/dcp394_path_sum.py
+++ tree/dcp394_path_sum.py
@@ -12,8 +12,17 @@ For example, given k = 18 and the following binary tree:
   4   13
  / \   \
 2   6   19
-Return True since the path 8 -> 4 -> 6 sums to 18.
 
+Return True since the path 8 -> 4 -> 6 sums to 18.
+"""
+
+"""
+112. Path Sum
+Easy
+
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
 """
 
 class TreeNode:
@@ -49,6 +58,8 @@ def print_tree(root, depth=0):
 
 ###############################################################################
 """
+Solution #1: recursion
+
 Finds the first leaf that gives the target sum along the path from root to leaf
 """
 def path_target_sum(root, target_sum, sum=0):
@@ -57,11 +68,8 @@ def path_target_sum(root, target_sum, sum=0):
 
     sum += root.val
 
-    if not root.left and not root.right: # leaf
-        if sum == target_sum:
-            return root
-        #else:
-        #    return None
+    if not root.left and not root.right and sum == target_sum:
+        return root
     
     leaf = path_target_sum(root.left, target_sum, sum)
     if leaf:
@@ -70,6 +78,28 @@ def path_target_sum(root, target_sum, sum=0):
     leaf = path_target_sum(root.right, target_sum, sum)
     if leaf:
         return leaf
+
+"""
+Solution #2: same but decrement target_sum instead of passing path_sum.
+"""
+def path_target_sum2(root, target_sum):
+    if root is None:
+        return None
+
+    target_sum -= root.val
+
+    if not root.left and not root.right and target_sum == 0:
+        return root
+    
+    leaf = path_target_sum2(root.left, target_sum)
+    if leaf:
+        return leaf
+
+    leaf = path_target_sum2(root.right, target_sum)
+    if leaf:
+        return leaf
+
+###############################################################################
 
 def path_to_node(root, node):
     # define inner path_dfs() so path variable is initialized to [] each time
@@ -87,11 +117,30 @@ def path_to_node(root, node):
     # path = [] # can also do this instead of having "path=[]" in path_dfs()
     return path_dfs(root, node)
 
+
+# Alternative
+def path_to_node2(root, node, path=None):
+    if path is None: # mutable so don't use default arg [] for path
+        path = [] # create new object each time fn is called outside recursion
+
+    if root is None:
+        return None
+
+    if (root == node 
+        or path_to_node2(root.left, node, path) 
+        or path_to_node2(root.right, node, path)
+    ):
+        path.append(root.val)
+        return path
+
+    return None
+
 ###############################################################################
 
 if __name__ == "__main__":
     def test(root, target_sum):
         leaf = path_target_sum(root, target_sum)
+        #leaf = path_target_sum2(root, target_sum)
         
         print("\n" + "#"*80)
         print_tree(root)
